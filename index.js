0baad9c6239de9d15ada34dfc260d0b8ab6ad68c
Andrew Kibiwott <kibiandrew@gmail.com>
2022-12-07 21:35:13 +0300

    Completed lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+const gifts=["Guadalupe", "Ollie", "Aki"];
+function writeCards(){
+    const response=[];
+    let j;
+    for(j=0; j<gifts.length; j++){
+       response[j]=`Thank you, ${gifts[j]}, for the wonderful surprise gift!`
+      
+    }
+    return response;
+}
+function countDown(){
+    let j=10
+    while(j>=0){
+        console.log(j--);
+
+    }
+}
+countDown()
